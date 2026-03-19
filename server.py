11e48a546aaa5f4d5382c13c4d950ef64e5c41a0
kpvkpiyush7-cmd <kpvkpiyush7@gmail.com>
2026-03-19 12:51:23 +0530

    Add /version endpoint to server
    Added a new endpoint to retrieve the version of the API.

--- server.py
+++ server.py
@@ -34,5 +34,14 @@ def activate():
 
     return jsonify({"status": "error"})
 
+@app.route("/version", methods=["GET"])
+def version():
+    return jsonify({
+        "version": "1.0.1",
+        "url": "https://github.com/kpvkpiyush7-cmd/license-server/releases/download/v1.0.1/ABS.exe"
+    })
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+

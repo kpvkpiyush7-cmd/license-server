fcaf4278bc4dbdaf412d3c560a8ceb0ea75ec2f6
kpvkpiyush7-cmd <kpvkpiyush7@gmail.com>
2026-05-01 12:06:17 +0530

    Update server.py

--- server.py
+++ server.py
@@ -794,8 +794,8 @@ def activate_key():
 @app.route("/version", methods=["GET"])
 def version():
     return jsonify({
-        "version": "2.7.1",
-        "url": "https://github.com/kpvkpiyush7-cmd/license-server/releases/download/v2.7.1/update.zip"
+        "version": "2.7.5",
+        "url": "https://github.com/kpvkpiyush7-cmd/license-server/releases/download/v2.7.5/update.zip"
     })
 
 
